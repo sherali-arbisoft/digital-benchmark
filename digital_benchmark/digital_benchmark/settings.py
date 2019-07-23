8e9ddd4f6a2f0d3f186c1aa0bba4b01fb34d8ddd
Sher.Ali <sher@A002-00045.arbisoft.com>
2019-07-23 15:45:48 +0500

    register facebook_benchmark app into digital_benchmark project settings

--- digital_benchmark/digital_benchmark/settings.py
+++ digital_benchmark/digital_benchmark/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'facebook_benchmark.apps.FacebookBenchmarkConfig'
 ]
 
 MIDDLEWARE = [
