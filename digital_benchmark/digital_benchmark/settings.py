e4b1cfcb73decc56e00bf8be0454af8e22302138
maleemnadeem <aleemchougtai@gmail.com>
2019-07-26 22:29:43 +0500

    register twitter_benchmark app

--- digital_benchmark/digital_benchmark/settings.py
+++ digital_benchmark/digital_benchmark/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'facebook_benchmark.apps.FacebookBenchmarkConfig', #register facebook_benchmark app
+    'twitter_benchmark', #register twitter_benchmark app
 ]
 
 MIDDLEWARE = [
