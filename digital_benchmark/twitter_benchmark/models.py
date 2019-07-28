200b70f8434b1b79fcd684920eb00de0716b3561
aleemnadeem <muhammad.aleem@arbisoft.com>
2019-07-28 22:42:35 +0500

    created model class for twitter user api

--- digital_benchmark/twitter_benchmark/models.py
+++ digital_benchmark/twitter_benchmark/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class UserData(models.Model):
+    user_id = models.CharField(max_length=255)
+    user_name = models.TextField()
+    screen_name = models.CharField(max_length=255)
+    user_location = models.CharField(max_length=255)
+    description = models.TextField(default=None, blank=True, null=True )
+    followers_count = models.IntegerField()#The number of followers this account currently has
+    friends_count = models.IntegerField() #The number of users this account is following
+    listed_count = models.IntegerField() #The number of public lists that this user is a member of
+    favourites_count = models.IntegerField() #The number of Tweets this user has liked in the account’s lifetime
+    statuses_count = models.IntegerField() #The number of Tweets (including retweets) issued by the user
