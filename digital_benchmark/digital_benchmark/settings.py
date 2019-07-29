8d4619c76433490f6947909c882ab8be20578b9f
sherali-arbisoft <sher.ali@arbisoft.com>
2019-07-29 15:31:40 +0500

    integrate postgresql

--- digital_benchmark/digital_benchmark/settings.py
+++ digital_benchmark/digital_benchmark/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'digital_benchmark.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'digital_benchmark',
+        'USER': 'digital_benchmark',
+        'PASSWORD': 'digital_benchmark',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
