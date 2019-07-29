4acc219553835b0d558653cabdb0ee097aee59f9
sherali-arbisoft <sher.ali@arbisoft.com>
2019-07-29 12:39:05 +0500

    implements facebook login with redirect to /facebook_benchmark/home/

--- digital_benchmark/facebook_benchmark/views.py
+++ digital_benchmark/facebook_benchmark/views.py
@@ -1,32 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import View
 
-import facebook as fb
+from .data_provider import FacebookDataProvider
 
-version = '3.1'
 
 # Create your views here.
-def feed(request, access_token):
-    graph = fb.GraphAPI(access_token=access_token, version=version)
-    feed = graph.get_connections(id='me', connection_name='feed')
-    response = '<ul>'
-    response = []
-    for post in feed['data']:
-        response += '<li>'
-        for key in post.keys():
-            if key == 'id':
-                response += "<a href='/facebook_benchmark/post/" + post.get(key, '') + "/" + access_token + "'>" + post.get(key, '') + "</a> "
-            else:
-                response += post.get(key, '') + ' '
-        response += '</li>'
-    response += '</ul>'
-    return HttpResponse(response)
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'facebook_benchmark/login.html')
 
-def post(request, id, access_token):
-    graph = fb.GraphAPI(access_token=access_token, version=version)
-    post = graph.get_object(id=id)
-    response = '<ul><li>'
-    for key in post.keys():
-        response += post.get(key, '') + ' '
-    response += '</li></ul>'
-    return HttpResponse(response)
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'facebook_benchmark/home.html')
