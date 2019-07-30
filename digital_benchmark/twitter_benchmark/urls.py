9a97d1b613796d8149751dae03d9cb4949272e64
aleem <muhammad.aleem@arbisoft.com>
2019-07-30 23:13:01 +0500

    add login url path

--- digital_benchmark/twitter_benchmark/urls.py
+++ digital_benchmark/twitter_benchmark/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import  views
 
 urlpatterns = [
-    path('',views.index,name = 'index'),
+    path('',views.login,name = 'login'),
     path('<int:user_id>/user/',views.user,name = 'user'),
+    path('success/',views.success,name = 'success')
 ]
