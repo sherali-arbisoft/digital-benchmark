d57a07f06418e40c6fe101ac3adc73ad5c58caf3
Abdul.Azeem <abdul@A002-00112.arbisoft.com>
2019-07-31 18:58:40 +0500

    app user login, register, logout and respective urls added globaly

--- digital_benchmark/digital_benchmark/settings.py
+++ digital_benchmark/digital_benchmark/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'digital_benchmark.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- digital_benchmark/digital_benchmark/urls.py
+++ digital_benchmark/digital_benchmark/urls.py
@@ -17,10 +17,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
+from .views import home
+
+from accounts.views import login_view, register_view, logout_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('facebook_benchmark/', include('facebook_benchmark.urls')),
-    path('instagram_benchmark/', include('instagram_benchmark.urls'))
+    path('instagram_benchmark/', include('instagram_benchmark.urls')),
+    path('', home),
+    path('accounts/login/', login_view),
+    path('accounts/register/', register_view),
+    path('accounts/logout/', logout_view)
 ]
 
 if settings.DEBUG:
