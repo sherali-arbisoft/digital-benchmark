52e87761bbc0ff32212b6b272a2cf086ffe7a2e6
sherali-arbisoft <sher.ali@arbisoft.com>
2019-07-31 14:10:25 +0500

    update CreateUpdateMixin

--- digital_benchmark/facebook_benchmark/models.py
+++ digital_benchmark/facebook_benchmark/models.py
@@ -10,8 +10,8 @@ class SoftDeleteMixin(models.Model):
         abstract = True
 
 class CreateUpdateMixin(models.Model):
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated_at = models.DateTimeField(auto_now=True, editable=False)
 
     class Meta:
         abstract = True
