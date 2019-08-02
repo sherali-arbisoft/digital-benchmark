b4d712b9e672daf214a369cc8212c35806ecc851
sherali-arbisoft <sher.ali@arbisoft.com>
2019-08-02 11:01:23 +0500

    remove unnecessary class based view

--- digital_benchmark/facebook_benchmark/views.py
+++ digital_benchmark/facebook_benchmark/views.py
@@ -20,12 +20,6 @@ class LoginView(View):
         url = 'https://www.facebook.com/v{version}/dialog/oauth?client_id={app_id}&redirect_uri={redirect_uri}&scope={permissions}&response_type={response_type}&state={state}'.format(version=settings.FACEBOOK_GRAPH_API_VERSION, app_id=settings.FACEBOOK_APP_ID, redirect_uri=settings.FACEBOOK_LOGIN_SUCCESSFUL_REDIRECT_URI, permissions=','.join(settings.FACEBOOK_PERMISSIONS), response_type=settings.FACEBOOK_RESPONSE_TYPE, state=settings.FACEBOOK_STATE)
         return redirect(url)
 
-class CodeResponseView(View):
-    def get(self, request):
-        code = request.GET['code']
-        url = 'https://graph.facebook.com/v{version}/oauth/access_token?client_id={app_id}&redirect_uri={redirect_uri}&client_secret={app_secret}&code={code}'.format(version=settings.FACEBOOK_GRAPH_API_VERSION, app_id=settings.FACEBOOK_APP_ID, redirect_uri=settings.FACEBOOK_LOGIN_SUCCESSFUL_REDIRECT_URI, app_secret=settings.FACEBOOK_APP_SECRET, code=code)
-        return redirect(url)
-
 class LoginSuccessfulView(View):
     def get(self, request):
         code = request.GET['code']
