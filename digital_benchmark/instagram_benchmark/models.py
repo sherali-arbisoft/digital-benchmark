d9b99f01e1c936d41ddd0705e1b567173594684e
Abdul.Azeem <abdul@A002-00112.arbisoft.com>
2019-08-02 14:49:09 +0500

    instagram data models added, parser remaining

--- digital_benchmark/instagram_benchmark/models.py
+++ digital_benchmark/instagram_benchmark/models.py
@@ -1,21 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
 
-class InstagramUser(models.Model):
-    insta_uid=models.CharField(max_length=200, primary_key=True)
-    access_token=models.CharField(max_length=200)
-    full_name=models.CharField(max_length=200)
-    username=models.CharField(max_length=200)
+class SoftDeleteMixin(models.Model):
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+class CreateUpdateMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
+
+class InstagramUser(SoftDeleteMixin, CreateUpdateMixin):
+    insta_uid=models.CharField(max_length=255, primary_key=True)
+    app_user=models.ForeignKey(User, on_delete=models.CASCADE)
+    access_token=models.CharField(max_length=255)
+    full_name=models.CharField(max_length=255)
+    username=models.CharField(max_length=255)
     is_business = models.BooleanField(default=False)
 
 
-# {'access_token': '9246763228.4d8f538.aba23cc5b7be401b93243e34819de617',
-#  'user': 
-#  {'id': '9246763228',
-#   'username': 'az_snaps1',
-#    'profile_picture': 'https://scontent.cdninstagram.com/vp/67a6db7df8b4b5ac96c1f3c7829e1d8e/5DE3C093/t51.2885-19/s150x150/44656038_560998554345667_7634009540609966080_n.jpg?_nc_ht=scontent.cdninstagram.com', 
-#    'full_name': 'AZ Snaps', 
-#    'bio': 'Photos that I wanted to share.', 
-#    'website': '', 
-#    'is_business': True}} 
+class InstagramMediaInsight(SoftDeleteMixin, CreateUpdateMixin):
+    media_insight_id=models.CharField(max_length=255, primary_key=True)
+    insta_user_id=models.ForeignKey(InstagramUser, on_delete=models.CASCADE)
+    likes_count=models.IntegerField(default=0)
+    comments_count=models.IntegerField(default=0)
+    media_tags=models.CharField(max_length=255, blank=True)
+    media_caption = models.TextField(blank=True)
+    media_type=models.CharField(max_length=255)
+    people_tagged=models.CharField(max_length=255, blank=True)
+    filter_used=models.CharField(max_length=255, blank=True)
+    post_created_time=models.DateTimeField(default=timezone.now,blank=True)
+
+
+class InstagramUserMedia(SoftDeleteMixin, CreateUpdateMixin):
+    media_id=models.CharField(max_length=255, primary_key=True)
+    insta_user_id=models.ForeignKey(InstagramUser, on_delete=models.CASCADE)
+    media_insight_id=models.ForeignKey(InstagramMediaInsight, on_delete=models.CASCADE)
+    media_url=models.CharField(max_length=255)
--- digital_benchmark/instagram_benchmark/views.py
+++ digital_benchmark/instagram_benchmark/views.py
@@ -36,7 +36,7 @@ class LoginSuccessView(View):
 
         userdata=response.json()
         #print(userdata)
-        newIgUser=InstagramUser(insta_uid=userdata['user']['id'],access_token=userdata['access_token'],full_name=userdata['user']['full_name'],username=userdata['user']['username'],is_business=userdata['user']['is_business'])
+        newIgUser=InstagramUser(insta_uid=userdata['user']['id'],app_user_id=request.user.id,access_token=userdata['access_token'],full_name=userdata['user']['full_name'],username=userdata['user']['username'],is_business=userdata['user']['is_business'])
         newIgUser.save()
         return render(request,'firstpage.html',{'data':userdata})
 
