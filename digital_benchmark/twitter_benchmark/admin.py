4174a6446a1e7b52daf331ef051214b5ba9640c7
aleem <muhammad.aleem@arbisoft.com>
2019-08-05 09:26:08 +0500

    add auth token model

--- digital_benchmark/twitter_benchmark/admin.py
+++ digital_benchmark/twitter_benchmark/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import UserData
+from .models import UserData, AuthToken
+
 
 @admin.register(UserData)
 class UserDataAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AuthToken)
+class AuthTokenAdmin(admin.ModelAdmin):
     pass
