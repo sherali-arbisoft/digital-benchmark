37de7a02960ed137a4998310a63857e7ad97a792
aleem <muhammad.aleem@arbisoft.com>
2019-08-05 09:27:09 +0500

    added get_data , get_tweets method

--- digital_benchmark/twitter_benchmark/data_provider.py
+++ digital_benchmark/twitter_benchmark/data_provider.py
@@ -1,24 +1,35 @@
 from requests_oauthlib import OAuth1Session
 from django.conf import settings
+from .models import AuthToken
 
 
 class DataProvider:
 
-    def get_user_profile_data(self,verifier_token,consumer_key, consumer_secret,resource_owner_key,resource_owner_secret):
-        oauth = OAuth1Session(consumer_key,
-                              client_secret=consumer_secret,
+    def __init__(self, verifier_token, resource_owner_key):
+        self.verifier_token = verifier_token
+        obj = AuthToken.objects.get(resource_owner_key=resource_owner_key)
+        oauth = OAuth1Session(settings.CONSUMER_KEY,
+                              client_secret=settings.CONSUMER_SECRET,
                               resource_owner_key=resource_owner_key,
-                              resource_owner_secret=resource_owner_secret,
-                              verifier=verifier_token)
+                              resource_owner_secret=obj.resource_owner_secret,
+                              verifier=self.verifier_token)
         oauth_tokens = oauth.fetch_access_token(settings.ACCESS_TOKEN_URL)
 
-        access_token = oauth_tokens['oauth_token']
-        access_token_secret = oauth_tokens['oauth_token_secret']
+        self.access_token = oauth_tokens['oauth_token']
+        self.access_token_secret = oauth_tokens['oauth_token_secret']
 
-        # Make the request
-        oauth = OAuth1Session(consumer_key,
-                              client_secret=consumer_secret,
-                              resource_owner_key=access_token,
-                              resource_owner_secret=access_token_secret)
-        response = oauth.get("https://api.twitter.com/1.1/account/settings.json")
-        return response.text
+    def get_user_profile_data(self):
+        oauth = OAuth1Session(settings.CONSUMER_KEY,
+                              client_secret=settings.CONSUMER_SECRET,
+                              resource_owner_key=self.access_token,
+                              resource_owner_secret=self.access_token_secret)
+        response = oauth.get(settings.PROFILE_DATA_URL)
+        return response.json
+
+    def get_tweets(self):
+        oauth = OAuth1Session(settings.CONSUMER_KEY,
+                              client_secret=settings.CONSUMER_SECRET,
+                              resource_owner_key=self.access_token,
+                              resource_owner_secret=self.access_token_secret)
+        response = oauth.get(settings.TWEETS_URL)
+        return response.json
