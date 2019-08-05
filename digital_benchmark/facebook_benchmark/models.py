e86592ae54ac6a103b801b66095175d124a90003
sherali-arbisoft <sher.ali@arbisoft.com>
2019-08-05 11:44:09 +0500

    add dunder str in facebook profiloe model

--- digital_benchmark/facebook_benchmark/models.py
+++ digital_benchmark/facebook_benchmark/models.py
@@ -23,6 +23,9 @@ class FacebookProfile(SoftDeleteMixin, CreateUpdateMixin):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 class Brand(SoftDeleteMixin, CreateUpdateMixin):
     name = models.CharField(max_length=255)
 
