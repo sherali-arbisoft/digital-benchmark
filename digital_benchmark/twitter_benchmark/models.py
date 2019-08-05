09e346c04034beb6aaa4ac8005bf7a6464bce351
aleem <muhammad.aleem@arbisoft.com>
2019-08-05 09:27:35 +0500

    added auth token model

--- digital_benchmark/twitter_benchmark/models.py
+++ digital_benchmark/twitter_benchmark/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
+
 class UserData(models.Model):
     user_id = models.CharField(max_length=255)
     user_name = models.TextField()
     screen_name = models.CharField(max_length=255)
     user_location = models.CharField(max_length=255)
     description = models.TextField(default=None, blank=True, null=True )
-    followers_count = models.IntegerField()#The number of followers this account currently has
-    friends_count = models.IntegerField() #The number of users this account is following
-    listed_count = models.IntegerField() #The number of public lists that this user is a member of
-    favourites_count = models.IntegerField() #The number of Tweets this user has liked in the account’s lifetime
-    statuses_count = models.IntegerField() #The number of Tweets (including retweets) issued by the user
+    followers_count = models.IntegerField()  # The number of followers this account currently has
+    friends_count = models.IntegerField()  # The number of users this account is following
+    listed_count = models.IntegerField()  # The number of public lists that this user is a member of
+    favourites_count = models.IntegerField()  # The number of Tweets this user has liked in the account’s lifetime
+    statuses_count = models.IntegerField()  # The number of Tweets (including retweets) issued by the user
+
+
+class AuthToken(models.Model):
+    resource_owner_key = models.CharField(max_length=255)
+    resource_owner_secret = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.resource_owner_secret
