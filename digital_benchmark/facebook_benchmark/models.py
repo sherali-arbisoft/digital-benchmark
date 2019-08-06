60b334fb99bb7f99e633874cf3681d784f712971
sherali-arbisoft <sher.ali@arbisoft.com>
2019-08-06 17:57:06 +0500

    connect django-auth-user to facebook profile

--- digital_benchmark/facebook_benchmark/models.py
+++ digital_benchmark/facebook_benchmark/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+from django.contrib.auth.models import User
 
 from enum import Enum
 
@@ -22,6 +22,8 @@ class FacebookProfile(SoftDeleteMixin, CreateUpdateMixin):
     facebook_id = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255, null=True, blank=False)
     last_name = models.CharField(max_length=255, null=True, blank=False)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Facebook Profile'
