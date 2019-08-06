490c8e9fd0d56b787263facea8c122bd29f8ddad
sherali-arbisoft <sher.ali@arbisoft.com>
2019-08-06 12:24:57 +0500

    remove empty lines from admin.py

--- digital_benchmark/facebook_benchmark/admin.py
+++ digital_benchmark/facebook_benchmark/admin.py
@@ -2,75 +2,52 @@ from django.contrib import admin
 
 from .models import FacebookProfile, Page, Rating, Post, PostReaction, Comment, CommentReaction
 
-# Register your models here.
 @admin.register(FacebookProfile)
 class FacebookProfileAdmin(admin.ModelAdmin):
-    search_fields = ['first_name', 'last_name']
-    
+    search_fields = ['first_name', 'last_name']    
     date_hierarchy = 'created_at'
-
     empty_value_display = '--empty--'
 
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
     list_display = ('name', 'num_engagements', 'fan_count', 'unread_message_count', 'unread_notif_count', 'verification_status')
-
     list_filter = ['verification_status']
-
     search_fields = ['name']
-
     date_hierarchy = 'created_at'
-
     empty_value_display = '--empty--'
 
 @admin.register(Rating)
 class RatingAdmin(admin.ModelAdmin):
     list_display = ('review_text', 'rating', 'recommendation_type')
-
     list_filter = ['recommendation_type']
-
     search_fields = ['recommendation_type', 'review_text']
-
     date_hierarchy = 'created_time'
-
     empty_value_display = '--empty--'
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('message', 'story')
-
     search_fields = ['message', 'story']
-
     date_hierarchy = 'created_time'
-
     empty_value_display = '--empty--'
 
 @admin.register(PostReaction)
 class PostReactionAdmin(admin.ModelAdmin):
     list_display = ('from_id', 'get_reaction_type_display')
-
     list_filter = ['reaction_type']
-
     search_fields = ['from_id', 'reaction_type']
-
     empty_value_display = '--empty--'
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('from_id', 'message')
-
     search_fields = ['from_id','message']
-
     date_hierarchy = 'created_time'
-
     empty_value_display = '--empty--'
 
 @admin.register(CommentReaction)
 class CommentReactionAdmin(admin.ModelAdmin):
     list_display = ('from_id', 'get_reaction_type_display')
-
     list_filter = ['reaction_type']
-
     search_fields = ['from_id', 'reaction_type']
-
     empty_value_display = '--empty--'
