23bad538c27b6683081cf25a51efc4002d1cec1b
aleem <muhammad.aleem@arbisoft.com>
2019-08-07 23:37:52 +0500

    addedd new methods for tweets

--- digital_benchmark/twitter_benchmark/data_provider.py
+++ digital_benchmark/twitter_benchmark/data_provider.py
@@ -30,7 +30,7 @@ class DataProvider:
         tweets = []
         total_tweets = self.get_user_profile_data().get('statuses_count')
         first_params = {"trim_user": True,
-                        "count":  5
+                        "count":  settings.TWEETS_COUNT
                        }
         oauth = OAuth1Session(settings.CONSUMER_KEY,
                               client_secret=settings.CONSUMER_SECRET,
@@ -42,7 +42,7 @@ class DataProvider:
         n = total_tweets
         while n > 0:
             params = {"trim_user": True,
-                      "count": 5,
+                      "count": settings.TWEETS_COUNT,
                       "max_id": lowest_tweet_id,
                       "exclude_replies": True
                       }
@@ -51,8 +51,51 @@ class DataProvider:
             n = n-5
             lowest_tweet_id = self.get_min_id(next_tweets)
             tweets.append(next_tweets[1:])
-        return tweets
+        return tweets[0]
 
-    def get_min_id(self,data):
+    def get_others_tweets(self, screen_name):
+        tweets = []
+        total_tweets = self.get_other_tweet_count(screen_name)
+        first_params = {"trim_user": True,
+                        "count":  settings.TWEETS_COUNT,
+                        "screen_name": screen_name
+                       }
+        oauth = OAuth1Session(settings.CONSUMER_KEY,
+                              client_secret=settings.CONSUMER_SECRET,
+                              resource_owner_key=self.access_token,
+                              resource_owner_secret=self.access_token_secret)
+        first_response = oauth.get(settings.TWEETS_URL, params=first_params)
+        tweets.append(first_response.json())
+        lowest_tweet_id = self.get_min_id(first_response.json())
+        n = total_tweets
+        while n > 0:
+            params = {"trim_user": True,
+                      "count": settings.TWEETS_COUNT,
+                      "max_id": lowest_tweet_id,
+                      "screen_name": screen_name
+                      }
+            response = oauth.get(settings.TWEETS_URL, params=params)
+            next_tweets = response.json()
+            n = n-5
+            lowest_tweet_id = self.get_min_id(next_tweets)
+            tweets.append(next_tweets[1:])
+            others_tweets = {'tweets': tweets[0],
+                             'screen_name': screen_name
+                             }
+        return others_tweets
+
+    def get_other_tweet_count(self, screen_name):
+        first_params = {"trim_user": False,
+                        "count": 1,
+                        "screen_name": screen_name
+                        }
+        oauth = OAuth1Session(settings.CONSUMER_KEY,
+                              client_secret=settings.CONSUMER_SECRET,
+                              resource_owner_key=self.access_token,
+                              resource_owner_secret=self.access_token_secret)
+        response = oauth.get(settings.TWEETS_URL, params=first_params)
+        return response.json()[0]['user']['statuses_count']
+
+    def get_min_id(self, data):
         last_tweet = data[-1]
         return last_tweet.get('id')
