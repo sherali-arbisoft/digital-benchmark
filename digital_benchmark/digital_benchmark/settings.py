3be869d96ddd91e8add94ee7323ddc42444faa22
sherali-arbisoft <sher.ali@arbisoft.com>
2019-08-16 17:25:36 +0500

    register rest_framework as app

--- digital_benchmark/digital_benchmark/settings.py
+++ digital_benchmark/digital_benchmark/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'instagram_benchmark',
-    'facebook_benchmark', #register facebook_benchmark app
+    'facebook_benchmark', # register facebook_benchmark app
+    'accounts',
     'debug_toolbar', # register django-debug-toolbar
+    'rest_framework', #register django rest framework
 ]
 
 MIDDLEWARE = [
