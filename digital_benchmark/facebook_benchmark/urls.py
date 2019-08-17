81e8cf453ae32902615256b464b94b53c852ed97
sherali-arbisoft <sher.ali@arbisoft.com>
2019-08-17 18:56:56 +0500

    register api endpoints

--- digital_benchmark/facebook_benchmark/urls.py
+++ digital_benchmark/facebook_benchmark/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('login_successful/', views.LoginSuccessfulView.as_view(), name='login_successful'),
     path('home/', views.HomeView.as_view(), name='home'),
     path('load_page_data/<int:page_id>/', views.LoadPageDataView.as_view(), name='load_page_data'),
+    path('facebook_profiles/', views.FacebookProfileList.as_view(), name='facebook_profiles'),
+    path('pages/', views.PageList.as_view(), name='pages'),
+    path('pages/<int:id>/posts/', views.PostList.as_view(), name='page_posts'),
+    path('posts/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
 ]
