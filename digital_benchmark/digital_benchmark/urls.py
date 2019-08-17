f3b8b25234c7122e88d8ee7bfa9b90cc10cab3a7
sherali-arbisoft <sher.ali@arbisoft.com>
2019-08-17 16:13:53 +0500

    integrate simple jwt

--- digital_benchmark/digital_benchmark/urls.py
+++ digital_benchmark/digital_benchmark/urls.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 from . import views
 
 from accounts.views import LoginView,LogoutView,RegisterView
@@ -14,7 +20,10 @@ urlpatterns = [
     path('', views.HomeView.as_view()),
     path('accounts/login/', LoginView.as_view()),
     path('accounts/register/', RegisterView.as_view()),
-    path('accounts/logout/', LogoutView.as_view())
+    path('accounts/logout/', LogoutView.as_view()),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 if settings.DEBUG:
