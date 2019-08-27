8c4c02a79a4df70881548ccbd060f70e14e207b9
sherali-arbisoft <sher.ali@arbisoft.com>
2019-08-27 15:04:10 +0500

    remove reaction parser and add comment parser

--- digital_benchmark/facebook_benchmark/data_parser.py
+++ digital_benchmark/facebook_benchmark/data_parser.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 from datetime import datetime
 
-from .models import FacebookProfile, Page, Rating, Post, PostReaction, Comment, CommentReaction
+from .models import FacebookProfile, Page, Rating, Post, Comment
 
 class FacebookUserDataParser:
     def __init__(self, user_id, facebook_profile_id=None, *args, **kwargs):
@@ -75,80 +75,43 @@ class FacebookPageDataParser:
             setattr(page, item['name'], item['values'][0]['value'])
         page.save()
         return page
-    
-    def _get_all_post_reactions(self, post_reactions_response):
-        post_reactions = []
-        for reaction in post_reactions_response.get('data'):
-            post_reaction = PostReaction()
-            post_reaction.from_id = reaction.get('id')
-            post_reaction.reaction_type = reaction.get('type', 'NONE')
-            post_reaction.save()
-            post_reactions.append(post_reaction)
-        return post_reactions
-    
-    def _get_all_comment_reactions(self, comment_reactions_response):
-        comment_reactions = []
-        for reaction in comment_reactions_response.get('data'):
-            comment_reaction = CommentReaction()
-            comment_reaction.from_id = reaction.get('id')
-            comment_reaction.reaction_type = reaction.get('type', 'NONE')
-            comment_reaction.save()
-            comment_reactions.append(comment_reaction)
-        return comment_reactions
-    
-    def _set_all_comments(self, post_id, post_comments_response):
-        for comment in post_comments_response.get('data'):
-            post_comment = Comment()
-            post_comment.comment_id = comment.get('id')
-            post_comment.created_time = comment.get('created_time', None)
-            post_comment.from_id = comment.get('from', {}).get('id')
-            post_comment.message = comment.get('message')
-            post_comment.post_id = post_id
-            post_comment.save()
-            if 'reactions' in comment:
-                comment_reactions = self._get_all_comment_reactions(comment.get('reactions'))
-                post_comment.reactions.add(*comment_reactions)
-    
-    def parse_post_details(self, post_details_response, *args, **kwargs):
+
+    def parse_comment(self, post_id, comment_response):
+        comment = Comment()
+        comment.comment_id = comment_response.get('id')
+        comment.created_time = comment_response.get('created_time', None)
+        comment.from_id = comment_response.get('from', {}).get('id')
+        comment.message = comment_response.get('message')
+        comment.angry_total = comment_response.get('angry', {}).get('summary', {}).get('total_count', 0)
+        comment.haha_total = comment_response.get('haha', {}).get('summary', {}).get('total_count', 0)
+        comment.like_total = comment_response.get('like', {}).get('summary', {}).get('total_count', 0)
+        comment.love_total = comment_response.get('love', {}).get('summary', {}).get('total_count', 0)
+        comment.sad_total = comment_response.get('sad', {}).get('summary', {}).get('total_count', 0)
+        comment.wow_total = comment_response.get('wow', {}).get('summary', {}).get('total_count', 0)
+        comment.post_id = post_id
+        comment.save()
+        return comment
+
+    def parse_post(self, post_response, *args, **kwargs):
         post = Post()
-        post.backdated_time = post_details_response.get('backdated_time', None)
-        post.created_time = post_details_response.get('created_time', None)
-        post.is_eligible_for_promotion = post_details_response.get('is_eligible_for_promotion', False)
-        post.is_expired = post_details_response.get('is_expired', False)
-        post.is_hidden = post_details_response.get('is_hidden', False)
-        post.is_instagram_eligible = post_details_response.get('is_instagram_eligible', False)
-        post.is_popular = post_details_response.get('is_popular', False)
-        post.is_published = post_details_response.get('is_published', False)
-        post.message = post_details_response.get('message', None)
-        post.post_id = post_details_response.get('id')
-        post.promotion_status = post_details_response.get('promotion_status')
-        post.scheduled_publish_time = post_details_response.get('scheduled_publish_time', None)
-        post.shares = post_details_response.get('shares', {}).get('count', 0)
-        post.story = post_details_response.get('story', None)
-        post.timeline_visibility = post_details_response.get('timeline_visibility', 'NORMAL').upper()
-        post.updated_time = post_details_response.get('updated_time', None)
+        post.backdated_time = post_response.get('backdated_time', None)
+        post.created_time = post_response.get('created_time', None)
+        post.is_eligible_for_promotion = post_response.get('is_eligible_for_promotion', False)
+        post.is_expired = post_response.get('is_expired', False)
+        post.is_hidden = post_response.get('is_hidden', False)
+        post.is_instagram_eligible = post_response.get('is_instagram_eligible', False)
+        post.is_popular = post_response.get('is_popular', False)
+        post.is_published = post_response.get('is_published', False)
+        post.message = post_response.get('message', None)
+        post.post_id = post_response.get('id')
+        post.promotion_status = post_response.get('promotion_status')
+        post.scheduled_publish_time = post_response.get('scheduled_publish_time', None)
+        post.shares = post_response.get('shares', {}).get('count', 0)
+        post.story = post_response.get('story', None)
+        post.timeline_visibility = post_response.get('timeline_visibility', 'NORMAL').upper()
+        post.updated_time = post_response.get('updated_time', None)
+        for item in post_response.get('insights', {}).get('data'):
+            setattr(post, item['name'], item['values'][0]['value'])
         post.page_id = self.page_id
         post.save()
-        if 'reactions' in post_details_response:
-            post_reactions = self._get_all_post_reactions(post_details_response.get('reactions'))
-            post.reactions.add(*post_reactions)
-        if 'comments' in post_details_response:
-            self._set_all_comments(post.id, post_details_response.get('comments'))
         return post
-    
-    def parse_post_insights(self, post_id, post_insights_response, *args, **kwargs):
-        post, created = Post.objects.get_or_create(id=post_id, defaults={
-            'page_id': self.page_id
-        })
-        for item in post_insights_response['data']:
-            setattr(post, item['name'], item['values'][0]['value'])
-        post.save()
-        return post
-    
-    def parse_all_posts(self, all_posts_response, *args, **kwargs):
-        all_posts = []
-        for post_response in all_posts_response:
-            post = self.parse_post_details(post_response)
-            post = self.parse_post_insights(post_id=post.id, post_insights_response=post_response.get('insights'))
-            all_posts.append(post)
-        return all_posts
