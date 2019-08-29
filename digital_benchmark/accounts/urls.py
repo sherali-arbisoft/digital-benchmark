77faf76023e6fbd77d5f047e4be849f04328290b
Abdul.Azeem <abdul@A002-00112.arbisoft.com>
2019-08-29 14:53:59 +0500

    yet another change

--- digital_benchmark/accounts/urls.py
+++ digital_benchmark/accounts/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view()),
     path('logout/', views.LogoutView.as_view()),
     #api endpoints for frontend
-    path('signup/', views.Signup.as_view(), name='register_django_user'),
+    path('register', views.Signup.as_view(), name='register_django_user'),
     path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify', TokenVerifyView.as_view(), name='token_verify'),
 ]
