52197266cb83665a7b9ecb256c120a3f1ca650f7
Muhammad.Aleem <aleem@A003-00042.arbisoft.com>
2019-08-29 23:17:39 +0500

    added api urls

--- digital_benchmark/twitter_benchmark/urls.py
+++ digital_benchmark/twitter_benchmark/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from . import  views
+from . import views
 
 app_name = 'twitter_benchmark'
 
 urlpatterns = [
-    path('login/',views.LoginView.as_view()),
-    path('success/',views.SuccessView.as_view()),
-    path('auth/',views.AuthView.as_view(),name ='auth'),
+    path('login/', views.LoginView.as_view()),
+    path('success/', views.SuccessView.as_view()),
+    path('auth/', views.AuthView.as_view(), name='auth'),
+    path('othertweet/<screenname>', views.OtherTweetList.as_view(), name='other_tweet'),
+    path('usertweet/', views.UserTweetList.as_view(), name='user_tweet'),
+    path('userdata/', views.UserDataList.as_view(), name='user_tweet'),
 ]
