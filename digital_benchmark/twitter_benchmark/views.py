e335ce1c639015b89c25d9b50389dfcd01cd20fe
Muhammad.Aleem <aleem@A003-00042.arbisoft.com>
2019-09-01 17:14:44 +0500

    added new api

--- digital_benchmark/twitter_benchmark/views.py
+++ digital_benchmark/twitter_benchmark/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 from django.views import View, generic
 from requests_oauthlib import OAuth1Session
-from .models import AuthToken
+from .models import AuthToken, UserComment, UserData, OtherTweet, UserTweet
 from .data_provider import DataProvider
 from .data_parser import TwitterDataParser
+from .serializers import UserTweetSerializer, UserDataSerializer, OtherTweetSerializer
+
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
 class LoginView(generic.ListView):
@@ -32,9 +37,74 @@ class SuccessView(View):
     def get(self, request):
         obj = DataProvider(request.GET['oauth_verifier'], request.GET['oauth_token'])
         # for testing
+        #ehmadzubair
         data = obj.get_other_tweet('ehmadzubair')
-        #data = TwitterDataParser.parser_other_tweet(data=data)
-        print(data)
+        data = TwitterDataParser.parser_other_tweet(data)
+        #print(data)
         return render(request, 'success/index.html')
 
+
+class UserDataList(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def retrieve(self, request, pk=None):
+        queryset = UserData.objects.filter(app_user=self.request.user)
+        serializer = UserDataSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class UserTweetList(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserTweetSerializer
+
+    def get_queryset(self):
+        return UserTweet.objects.filter(app_user_id=self.request.user.id)
+
+
+class OtherTweetList(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = OtherTweetSerializer
+
+    def get_queryset(self):
+        return OtherTweet.objects.filter(screen_name=self.kwargs.get('screenname'))
+
+
+class UserTweetByIdList(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserTweetSerializer
+
+    def get_queryset(self):
+        return UserTweet.objects.filter(tweet_id=self.kwargs.get('id')).order_by('-created_at')
+
+
+class UserLatestTweet(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def retrieve(self, request, id):
+        try:
+            queryset = UserTweet.objects.filter(tweet_id=id).latest('created_at')
+            serializer = UserTweetSerializer(queryset)
+            return Response(serializer.data)
+        except UserData.DoesNotExist:
+            return Response({'detail': 'Not found.'})
+
+
+class OtherTweetByIdList(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = OtherTweetSerializer
+
+    def get_queryset(self):
+        return OtherTweet.objects.filter(tweet_id=self.kwargs.get('id')).order_by('-created_at')
+
+
+class OtherLatestTweet(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def retrieve(self, request, id):
+        try:
+            queryset = OtherTweet.objects.filter(tweet_id=id).latest('created_at')
+            serializer = OtherTweetSerializer(queryset)
+            return Response(serializer.data)
+        except OtherTweet.DoesNotExist:
+            return Response({'detail': 'Not found.'})
 
